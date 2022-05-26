fcf343234fc8966da52aa809ad928c19e1d60ed9
redthorx <55249192+redthorx@users.noreply.github.com>
2022-05-26 13:42:56 +0800

    Update SocketIoClient.h
    replaced std with arx & ustd drop-ins

--- SocketIoClient.h
+++ SocketIoClient.h
@@ -1,13 +1,20 @@
+#ifndef __ATMEGA__
+#define __MEGA__
+#define __ATMEGA__
+#endif
+#include "ustd_platform.h"
+#include "ustd_functional.h"
+
 #ifndef __SOCKET_IO_CLIENT_H__
 #define __SOCKET_IO_CLIENT_H__
-
+#include <ArxContainer.h>
 #include <Arduino.h>
-#include <map>
-#include <vector>
 #include <WebSocketsClient.h>
 
+
 #ifndef NODEBUG_SOCKETIOCLIENT
-   #define SOCKETIOCLIENT_DEBUG(...) Serial.printf(__VA_ARGS__);
+
+   #define SOCKETIOCLIENT_DEBUG(...) Serial.print("Test");
 #else
    #define SOCKETIOCLIENT_DEBUG(...)
 #endif
@@ -26,23 +33,23 @@
 
 class SocketIoClient {
 private:
-	std::vector<String> _packets;
+	arx::vector<String> _packets;
 	WebSocketsClient _webSocket;
 	int _lastPing;
-	std::map<String, std::function<void (const char * payload, size_t length)>> _events;
+	arx::map<String, ustd::function<void (const char * payload, size_t length)>> _events;
 
 	void trigger(const char* event, const char * payload, size_t length);
 	void webSocketEvent(WStype_t type, uint8_t * payload, size_t length);
     void initialize();
+	bool _disableHeartbeat=false;
 public:
-    void beginSSL(const char* host, const int port = DEFAULT_PORT, const char* url = DEFAULT_URL, const char* fingerprint = DEFAULT_FINGERPRINT);
 	void begin(const char* host, const int port = DEFAULT_PORT, const char* url = DEFAULT_URL);
 	void loop();
-	void on(const char* event, std::function<void (const char * payload, size_t length)>);
+	void on(const char* event, ustd::function<void (const char * payload, size_t length)>);
 	void emit(const char* event, const char * payload = NULL);
 	void remove(const char* event);
 	void disconnect();
-	void setAuthorization(const char * user, const char * password);
+	void configureEIOping(bool disableHeartbeat);
 };
 
 #endif
